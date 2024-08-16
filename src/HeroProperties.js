82b81e146a906d2e00be2a467ce410f7582884f0
kamshir <iammaksim2000shirshikov@gmail.com>
2024-08-16 16:41:50 +0300

    HeroProperties

---
+++ src/HeroProperties.js
@@ -0,0 +1,51 @@
+// src/HeroProperties.js
+import React from 'react';
+
+const HeroProperties = ({ selectedHero, onChange }) => {
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    onChange({ ...selectedHero, [name]: value });
+  };
+
+  return (
+    <div>
+      {selectedHero && (
+        <div>
+          <h2>Hero's Properties</h2>
+          <label>
+            Цвет:
+            <input
+              type="color"
+              name="color"
+              value={selectedHero.color}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br />
+          <label>
+            Speed X:
+            <input
+              type="number"
+              name="speedX"
+              value={selectedHero.speedX}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br />
+          <label>
+            Speed Y:
+            <input
+              type="number"
+              name="speedY"
+              value={selectedHero.speedY}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HeroProperties;
